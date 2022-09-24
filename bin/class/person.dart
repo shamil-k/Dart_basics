987a7d511b03c366de01e401013805fe375963c1
shamil-k <bareabhilash2000@gmail.com>
2022-09-24 23:16:28 +0530

    set method sample added

--- bin/class/person.dart
+++ bin/class/person.dart
@@ -1,14 +1,28 @@
 class Person {
   final String _name;
-  final int _age;
+  int _age;
 
   Person(this._name, this._age);
 
-  int getAge() {
+  //named constructor
+
+  Person.above50(this._name, this._age);
+  Person.below50(this._name, this._age);
+
+
+  int getAgefunction() {
     return _age;
   }
 
   String get name {
     return _name;
+  }
+
+  set setAge(int value) {
+    _age = value;
+  }
+
+  int get getAge {
+    return _age;
   }
 }
